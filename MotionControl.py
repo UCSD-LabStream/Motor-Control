87bfd08d776c188fee243f1b732d5a92197f0433
Prash <prashk135@gmail.com>
2024-12-16 15:32:04 -0800

    Thorlabs KDC101 motor control script

---
+++ MotionControl.py
@@ -0,0 +1,161 @@
+import time
+import os
+import sys
+from ctypes import *
+import requests
+
+IN_PROGRESS_URL = "http://13.57.216.198/motor_state/1/in_progress"
+DONE_URL = "http://13.57.216.198/motor_state/1/done"
+GET_URL =  "http://13.57.216.198/get_gears/1"
+
+
+def main():
+    """
+    main():
+    ------
+
+    Performs all actions of the KDC101
+    :return: None
+    """
+
+    if sys.version_info < (3, 8):
+        os.chdir(r"C:\Program Files\Thorlabs\Kinesis")
+    else:
+        os.add_dll_directory(r"C:\Program Files\Thorlabs\Kinesis")
+
+    lib: CDLL = cdll.LoadLibrary("Thorlabs.MotionControl.KCube.DCServo.dll")
+
+    # Uncomment this line if you are using simulations
+    #lib.TLI_InitializeSimulations()
+
+    # Set constants
+    serial_num = c_char_p(b"27262362")
+
+    # Open the device
+    if lib.TLI_BuildDeviceList() == 0:
+        lib.CC_Open(serial_num)
+        lib.CC_StartPolling(serial_num, c_int(200))
+
+        # Home the device
+        lib.CC_Home(serial_num)
+        time.sleep(1)
+
+        # Set up the device to convert real units to device units
+        STEPS_PER_REV = c_double(2000.00)  # for the PRM1-Z8
+        STEPS_PER_DEGREE = 2000.00/360.00
+        gbox_ratio = c_double(1.0)  # gearbox ratio
+        pitch = c_double(1.0)
+
+        # Apply these values to the device
+        lib.CC_SetMotorParamsExt(serial_num, STEPS_PER_REV, gbox_ratio, pitch)
+
+        # Get the device's current position in dev units
+        lib.CC_RequestPosition(serial_num)
+        time.sleep(0.2)
+        dev_pos = c_int(lib.CC_GetPosition(serial_num))
+        # Convert device units to real units
+        real_pos = c_double()
+        lib.CC_GetRealValueFromDeviceUnit(serial_num,
+                                          dev_pos,
+                                          byref(real_pos),
+                                          0)
+
+        print(f'Position after homing: {real_pos.value}')
+        while True:
+            #get user input for rotation amount
+            current_local_time = time.localtime()
+            if current_local_time.tm_hour >= 24:
+                break
+            # Replace with your API endpoint
+           
+            data = 0
+            try:
+                # Send a GET request
+                response = requests.get(GET_URL)
+
+                # Check if the request was successful
+                if response.status_code == 200:
+                    # Parse the JSON response if applicable
+                    data = response.json()
+                    print("Value retrieved:", data)  # Replace 'your_key' with the actual key
+                else:
+                    print(f"Failed to retrieve data: {response.status_code} - {response.text}")
+                    continue
+            except requests.RequestException as e:
+                print(f"An error occurred: {e}")
+            
+            user_input = float(data) #not sure this is how it works, need to characterize the motor
+            if user_input  < 0: 
+                continue
+            # set a new position in device units
+            new_pos_real = c_double(user_input)  # in real units
+
+            dev_pos1 = c_int(lib.CC_GetPosition(serial_num))
+            real_pos = c_double()
+            lib.CC_GetRealValueFromDeviceUnit(serial_num,
+                                           dev_pos1,
+                                           byref(real_pos),
+                                           0)
+
+            if (real_pos.value != user_input):
+                new_pos_dev = c_int()
+                lib.CC_GetDeviceUnitFromRealValue(serial_num,
+                                            new_pos_real,
+                                            byref(new_pos_dev),
+                                            0)
+                
+                print(f'{new_pos_real.value} in Device Units: {new_pos_dev.value}')
+            
+                response = requests.post(IN_PROGRESS_URL)
+                # print(response.status_code)
+                while(not response.status_code == 200):
+                    response = requests.post(IN_PROGRESS_URL)
+                print("finished sending response")
+
+                # Move to new position as an absolute move.
+                lib.CC_SetMoveAbsolutePosition(serial_num, new_pos_dev)
+                time.sleep(0.25)
+                lib.CC_MoveAbsolute(serial_num)
+                dev_pos1 = c_int(lib.CC_GetPosition(serial_num))
+                real_pos = c_double()
+                lib.CC_GetRealValueFromDeviceUnit(serial_num,
+                                            dev_pos1,
+                                            byref(real_pos),
+                                            0)
+                while(real_pos.value != user_input):
+                    time.sleep(0.5)
+                    dev_pos2 = c_int(lib.CC_GetPosition(serial_num))
+                    real_pos = c_double()
+                    lib.CC_GetRealValueFromDeviceUnit(serial_num,
+                                                dev_pos2,
+                                                byref(real_pos),
+                                                0)
+                print("finished moving")
+
+                response = requests.post(DONE_URL)
+                while(not response.status_code == 200):
+                    response = requests.post(DONE_URL)
+                
+        # time.sleep(0.75)
+        #reset position when done
+        new_pos_real = c_double(0)  # in real units
+        new_pos_dev = c_int()
+        lib.CC_GetDeviceUnitFromRealValue(serial_num,
+                                        new_pos_real,
+                                        byref(new_pos_dev),
+                                        0)
+
+        # Move to new position as an absolute move.
+        lib.CC_SetMoveAbsolutePosition(serial_num, new_pos_dev)
+        time.sleep(0.25)
+        lib.CC_MoveAbsolute(serial_num)
+
+        # Close the device
+        lib.CC_Close(serial_num)
+    #lib.TLI_UninitializeSimulations()
+
+    return
+
+
+if __name__ == "__main__":
+    main()
